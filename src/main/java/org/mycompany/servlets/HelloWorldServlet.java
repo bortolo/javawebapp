017b7b09b71be8b03a8e04e9ce8e47d52fcfb68e
Andrea <andrea.bortolossi89@gmail.com>
2020-07-21 15:12:33 +0200

    First webapp java maven .war

---
+++ src/main/java/org/mycompany/servlets/HelloWorldServlet.java
@@ -0,0 +1,41 @@
+package org.mycompany.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Date;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/saytime")
+public class HelloWorldServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("Hellow world: " + new Date().toString());
+    }
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+}
